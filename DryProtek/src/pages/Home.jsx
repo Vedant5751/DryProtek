ac483e15b7790398b6145ed19f02baa182846d54
Vedant Patil <vedant1346@gmail.com>
2024-02-09 22:18:26 +0530

    BluePrint Home Page

--- DryProtek/src/pages/Home.jsx
+++ DryProtek/src/pages/Home.jsx
@@ -5,42 +5,14 @@ function Home() {
   return (
     <>
       <div>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
-        <h1 className="text-red-500 font-bold underline">Hello world!</h1>
+        <h1 className="bg-green-500 font-bold underline">Carousel</h1>
+        <h1 className="bg-red-500 font-bold underline">Floating Navbar</h1>
+        <h1 className="bg-blue-500 font-bold underline">Applications</h1>
+        <h1 className="bg-orange-500 font-bold underline">Product News</h1>
+        <h1 className="bg-gray-500 font-bold underline">Product Articles</h1>
+        <h1 className="bg-yellow-500 font-bold underline">Products</h1>
+        <h1 className="bg-cyan-500 font-bold underline">Products</h1>
+        <h1 className="bg-lime-500 font-bold underline">Products</h1>
       </div>
     </>
   );
